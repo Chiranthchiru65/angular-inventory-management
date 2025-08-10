1662bfac34b91459474e0961066dbf9c0871d81a
Chiranthchiru65 <chiranthchiru65@gmail.com>
2025-08-10 23:02:01 +0530

    1.3 routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Form } from './pages/form/form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'form', component: Form },
+];
--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [MatInputModule, MatIconModule],
+  imports: [MatInputModule, MatIconModule, RouterModule],
   templateUrl: './navbar.html',
 })
 export class Navbar {}
