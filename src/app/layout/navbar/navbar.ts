d7e1c0a9905ca4af8606fee9ee3cea49d6ff668e
Chiranthchiru65 <chiranthchiru65@gmail.com>
2025-08-10 15:27:46 +0530

    1.2

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatInputModule, MatIconModule],
   templateUrl: './navbar.html',
-  // styleUrl: './navbar.css'
 })
 export class Navbar {}
