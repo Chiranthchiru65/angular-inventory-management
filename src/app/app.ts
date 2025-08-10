96a62bf247f516dc1ee9069d8c535736704a5c54
Chiranthchiru65 <chiranthchiru65@gmail.com>
2025-08-10 15:01:44 +0530

    1.1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { Dashboard } from './layout/dashboard/dashboard';
 
 @Component({
@@ -7,6 +7,4 @@ import { Dashboard } from './layout/dashboard/dashboard';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('angular-inventory');
-}
+export class App {}
