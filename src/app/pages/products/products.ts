8e30e85b3b0b5159e2f0bbb17b00020bfe970804
Chiranthchiru65 <chiranthchiru65@gmail.com>
2025-08-13 12:12:26 +0530

    1.5 latest

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -9,6 +9,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-products',
@@ -27,4 +29,33 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
-export class Products {}
+export class Products implements OnInit {
+  products: Product[] = [];
+  loading = false;
+  error: string | null = null;
+  constructor(private productService: ProductService) {}
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.loading = true;
+    this.error = null;
+
+    this.productService.getProducts().subscribe({
+      next: (data: Product[]) => {
+        this.products = data;
+        console.log('fetched data :', this.products);
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('err :', error);
+        this.error = 'failed to load data';
+        this.loading = false;
+      },
+      complete: () => {
+        console.log('fetched data complete');
+      },
+    });
+  }
+}
